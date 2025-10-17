a3f0a2c32da6902d7ffd322564745e444d8e47fc
LesF25 <getd8774@gmail.com>
2025-10-17 22:51:09 +0700

    Add csv_reader

---
+++ app/core/file_readers/csv_reader.py
@@ -0,0 +1,15 @@
+import csv
+from typing import Any, Generator
+
+from .reader import file_reader, BaseFileReader
+
+
+@file_reader('.csv')
+class CsvReader(BaseFileReader):
+    def read_file(
+        self,
+        file_name: str,
+    ) -> Generator[dict[str, Any], None, None]:
+        with open(file_name, mode='r') as file:
+            for row in csv.DictReader(file):
+                yield row
